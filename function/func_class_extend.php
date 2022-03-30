10e93dc461054c819648a63cda895723406d74f2
Farkhad Khamidullin <8kiloparsec@gmail.com>
2022-03-31 00:08:30 +0300

    test commit

--- function/func_class_extend.php
+++ function/func_class_extend.php
@@ -1,7 +1,7 @@
 <?php
     echo "<a href='../php2book/form1.html'>Главная страница</a><br>"; 
-    // ini_set('display_errors',1);
-    // error_reporting(E_ALL ^E_NOTICE);
+    ini_set('display_errors',1);
+    error_reporting(E_ALL ^E_NOTICE);
 
     
 
