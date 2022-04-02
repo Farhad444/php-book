0d15c5c7ccfebd54c5cdd2d2f5adfde500546687
Farkhad Khamidullin <8kiloparsec@gmail.com>
2022-04-02 23:35:28 +0300

    learning class extension (изучение расширения класса)

--- function/func_class_extend.php
+++ function/func_class_extend.php
@@ -1,8 +1,25 @@
 <?php
-    echo "<a href='../php2book/form1.html'>Главная страница</a><br>"; 
-    ini_set('display_errors',1);
-    error_reporting(E_ALL ^E_NOTICE);
 
+use Entree as GlobalEntree;
+
+    echo "<a href='../php2book/form1.html'>Главная страница</a><br>"; 
+    // ini_set('display_errors',1);
+    // error_reporting(E_ALL ^E_NOTICE);
+
+    class Entree{
+        public $name;
+        public $ingredients = array();
+        public function __construct($val_1, $val_2){
+            $this->name = $val_1;
+            $this->ingredients = $val_2;
+        }
+        public function hasIngredient($ingredient){
+            return in_array($ingredient, $this->ingredients);
+        }
+    }
+    $soup = new Entree('Chicken Soup', ['chicken', 'water']);
+    $sandwich = new Entree('Chicken Sandwich', ['chicken', 'bread']);
+    
     
 
 
